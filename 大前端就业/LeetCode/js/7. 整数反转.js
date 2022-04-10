3f50224419caf205cc9b130dfc445ad41ebe51ef
yanding.li <David.Jackson.Lyd@gmail.com>
2022-04-10 15:12:47 +0800

    7. 整数反转

--- 大前端就业/LeetCode/js/7. 整数反转.js
+++ 大前端就业/LeetCode/js/7. 整数反转.js
@@ -26,9 +26,9 @@ var overflowNum = (num) => {
 var reverse2 = function(x) {
     let rev = 0;
     while (x !== 0) {
-        const digit = x % 10;
-        x = ~~(x / 10);
-        rev = rev * 10 + digit;
+        const digit = x % 10; // 得到余数 -4 -2 -3
+        x = ~~(x / 10); // 得到除数 -32 -3 0
+        rev = rev * 10 + digit; // 得到新的翻转后的数 0*10 + -4 = -4， -4*10 + -2 = -42，    -42*10 + -3 = -423
         if (rev < Math.pow(-2, 31) || rev > Math.pow(2, 31) - 1) {
             return 0;
         }
